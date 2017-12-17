ecad132b38cf36b3e99443954d111c41e591b5f3
dbohdan <dbohdan@dbohdan.com>
2017-12-17 23:07:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+The following is a list of tools that automatically expose a REST, GraphQL, or another kind of API for your database.
+
+|                     Project name/link                     |                                       Database(s) supported                                       | API type | Implementation language |                  License                  |          GitHub stats           |   Notes    |
+|-----------------------------------------------------------|---------------------------------------------------------------------------------------------------|----------|-------------------------|-------------------------------------------|---------------------------------|------------|
+| [Datasette](https://github.com/simonw/datasette)          | SQLite 3                                                                                          | REST     | Python 3                | Apache 2.0                                | 1091&nbsp;★, 253&nbsp;commits   | Read-only. |
+| [DreamFactory](https://www.dreamfactory.com/)             | MySQL, PostgreSQL, SQLite, MongoDB, CouchDB, and [others](https://www.dreamfactory.com/products). | REST     | PHP 5                   | Apache 2.0, proprietary (optional extras) | n/a                             |            |
+| [PostGraphQL](https://github.com/postgraphql/postgraphql) | PostgreSQL                                                                                        | GraphQL  | TypeScript (Node.js)    | MIT                                       | 4484&nbsp;★, 664&nbsp;commits   |            |
+| [PostgREST](https://github.com/begriffs/postgrest)        | PostgreSQL                                                                                        | REST     | Haskell                 | MIT                                       | 10049&nbsp;★, 1351&nbsp;commits |            |
+| [sandman2](https://github.com/jeffknupp/sandman2)         | All supported by SQLAlchemy (MySQL, PostgreSQL, SQLite, Oracle, MS SQL, and others).              | REST     | Python 2/3              | Apache 2.0                                | 641&nbsp;★, 129&nbsp;commits    |            |
+| [tuql](https://github.com/bradleyboy/tuql)                | SQLite 3                                                                                          | GraphQL  | JavaScript (Node.js)    | MIT                                       | 174&nbsp;★, 34&nbsp;commits     | Read-only. |
+| [xmysql](https://github.com/o1lab/xmysql)                 | MySQL                                                                                             | REST     | JavaScript (Node.js)    | MIT                                       | 1672&nbsp;★, 205&nbsp;commits   |            |
+
+
+GitHub stats updated 2017-12-18.
+
+# Contributing
+
+Your contributions are welcome! Please submit a pull request or create an issue to add a new project to the list.
+
+# License
+
+This document is licensed under the [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/). By contributing you agree to release your contribution under this license.
---
+++ render-template.go
@@ -0,0 +1,280 @@
+// A script to generate a README.md from README.md.template and the data
+// in data/. It fetches statistics about the repositories from GitHub.
+// Copyright (c) dbohdan, 2017.
+// License: MIT.
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"text/template"
+	"time"
+
+	"github.com/olekukonko/tablewriter"
+	"gopkg.in/yaml.v2"
+)
+
+type entry struct {
+	Name    string
+	URL     string
+	DB      string
+	API     string
+	Lang    string
+	License string
+	Stats   string
+	Notes   string
+}
+type table [][]string
+type repo struct {
+	Name  string
+	Owner string
+}
+type projStats struct {
+	Name string
+	Ref  struct {
+		Target struct {
+			History struct {
+				TotalCount int
+			}
+		}
+	}
+	Stargazers struct {
+		TotalCount int
+	}
+}
+type gitHubResponse struct {
+	Data    map[string]projStats
+	Message string
+	Errors  []map[string]interface{}
+}
+
+// Load entry data from the YAML files that match the glob pattern.
+func loadEntries(glob string) (entries []entry, err error) {
+	matches, err := filepath.Glob(glob)
+	if err != nil {
+		return nil, err
+	}
+	entries = make([]entry, len(matches))
+	for i, match := range matches {
+		data, err := ioutil.ReadFile(match)
+		if err != nil {
+			return nil, err
+		}
+		yaml.Unmarshal(data, &entries[i])
+	}
+	return entries, nil
+}
+
+// Convert each entry into a table row.
+func entriesToTable(entries []entry) (tbl table) {
+	tbl = make(table, len(entries)+1)
+	tbl[0] = make([]string, 7)
+	tbl[0][0] = "Project name/link"
+	tbl[0][1] = "Database(s) supported"
+	tbl[0][2] = "API type"
+	tbl[0][3] = "Implementation language"
+	tbl[0][4] = "License"
+	tbl[0][5] = "GitHub stats"
+	tbl[0][6] = "Notes"
+
+	for i, ent := range entries {
+		j := i + 1
+		tbl[j] = make([]string, 7)
+		tbl[j][0] = fmt.Sprintf("[%s](%s)",
+			html.EscapeString(ent.Name),
+			html.EscapeString(ent.URL))
+		tbl[j][1] = html.EscapeString(ent.DB)
+		tbl[j][2] = html.EscapeString(ent.API)
+		tbl[j][3] = html.EscapeString(ent.Lang)
+		tbl[j][4] = html.EscapeString(ent.License)
+		// Deliberately allow HTML in Stats and Notes.
+		tbl[j][5] = ent.Stats
+		tbl[j][6] = ent.Notes
+	}
+	return tbl
+}
+
+// Convert a table to its Markdown representation. Each column in the returned
+// Markdown will have a consistent width to be more readable as plain text.
+func (tbl table) Format() (res string) {
+	buffer := bytes.NewBufferString("")
+	tw := tablewriter.NewWriter(buffer)
+	tw.SetHeader(tbl[0])
+	tw.SetAutoFormatHeaders(false)
+	tw.SetAutoWrapText(false)
+	tw.SetBorders(tablewriter.Border{
+		Left:   true,
+		Top:    false,
+		Right:  true,
+		Bottom: false})
+	tw.SetCenterSeparator("|")
+	tw.AppendBulk(tbl[1:])
+	tw.Render()
+	return buffer.String()
+}
+
+// Perform a GraphQL query against the GitHub API v4.
+func queryGitHub(token string, query string) (body []byte, err error) {
+	url := "https://api.github.com/graphql"
+
+	wrapper := map[string]string{}
+	wrapper["query"] = query
+	jsonReq, err := json.Marshal(wrapper)
+	if err != nil {
+		return nil, err
+	}
+	jsonReqBuf := bytes.NewBuffer([]byte(jsonReq))
+
+	req, err := http.NewRequest("POST", url, jsonReqBuf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("bearer %s", token))
+	req.Header.Set("Content-Type", "application/graphql")
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+// Build a GraphQL query to fetch the GitHub repository statistics for several
+// repositories at once.
+func buildStatsQuery(repos []repo) (query string, err error) {
+	repoQuery := `r%d: repository(owner: "%s", name: "%s") { ...ProjStats }`
+	projStatsFragment := `
+		fragment ProjStats on Repository {
+		  name
+		  ref(qualifiedName: "master") {
+		    target {
+		      ... on Commit {
+		        history {
+		          totalCount
+		        }
+		      }
+		    }
+		  }
+		  stargazers {
+		    totalCount
+		  }
+		}
+	`
+	queryBuf := bytes.NewBufferString("{\n")
+	safe, err := regexp.Compile("^[a-zA-Z0-9_-]+$")
+	if err != nil {
+		return "", err
+	}
+	for i, r := range repos {
+		// We do not make a fuss about empty repository entries. We
+		// simply skip them here and return an empty string for them at
+		// the end.
+		if r.Name != "" {
+			if !(safe.MatchString(r.Owner) &&
+				safe.MatchString(r.Name)) {
+				return "", fmt.Errorf("Bad repo data: %v", r)
+			}
+			t := fmt.Sprintf(repoQuery, i, r.Owner, r.Name)
+			queryBuf.WriteString(t)
+			queryBuf.WriteString("\n")
+		}
+	}
+	queryBuf.WriteString("}\n")
+	queryBuf.WriteString(projStatsFragment)
+
+	return queryBuf.String(), nil
+}
+
+// Fetch the repository statistics (the number of stars and commits) for the
+// repositories in repos and return them formatted as HTML.
+func fetchGitHubStats(token string, repos []repo) (statsHTML []string,
+	err error) {
+	query, err := buildStatsQuery(repos)
+	if err != nil {
+		return nil, err
+	}
+	respBody, err := queryGitHub(token, query)
+	if err != nil {
+		return nil, err
+	}
+	var respData gitHubResponse
+	err = json.Unmarshal(respBody, &respData)
+	if err != nil {
+		return nil, err
+	}
+	if respData.Message != "" {
+		return nil, fmt.Errorf("GitHub API: %s", respData.Message)
+	}
+	if len(respData.Errors) > 0 {
+		return nil, fmt.Errorf("GitHub API: %s",
+			respData.Errors[0]["message"].(string))
+	}
+	statsHTML = make([]string, len(repos))
+	for k, v := range respData.Data {
+		i, err := strconv.Atoi(k[1:])
+		if err != nil {
+			return nil, err
+		}
+		statsHTML[i] = fmt.Sprintf("%d&nbsp;★, %d&nbsp;commits",
+			v.Stargazers.TotalCount,
+			v.Ref.Target.History.TotalCount)
+	}
+	return statsHTML, nil
+}
+
+func main() {
+	tmpl, err := template.ParseFiles("README.md.template")
+	if err != nil {
+		log.Fatal(err)
+	}
+	entries, err := loadEntries("data/*.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	re, err := regexp.Compile(
+		"https?://github.com/([a-z0-9-]+)/([a-zA-Z0-9_-]+)/?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	repos := make([]repo, len(entries))
+	for i, ent := range entries {
+		found := re.FindStringSubmatch(ent.URL)
+		if len(found) == 3 {
+			repos[i].Owner = found[1]
+			repos[i].Name = found[2]
+		} else {
+			entries[i].Stats = "n/a"
+		}
+	}
+	statsHTML, err := fetchGitHubStats(os.Getenv("GITHUB_TOKEN"), repos)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, s := range statsHTML {
+		if s != "" {
+			entries[i].Stats = s
+		}
+	}
+
+	tbl := entriesToTable(entries)
+	dot := map[string]interface{}{}
+	dot["date"] = time.Now().Format("2006-01-02")
+	dot["table"] = tbl.Format()
+
+	tmpl.Execute(os.Stdout, dot)
+}
